f4db7a3ee1f4d517d86d7be56a068db4e09e0705
macmule <bentoms@btopenworld.com>
2014-11-19 22:16:54 +0000

    Update contents.sh

--- contents.sh
+++ contents.sh
@@ -448,4 +448,4 @@ background:rgba(0,0,0,0.35)
 	
 security find-generic-password -l "Safari Extensions List" -g
 
-$loggedinUser = `python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
